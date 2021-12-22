c308e9d05d385fa09ab399009fb05dd47df6e4ff
gb_yueqifeng <gb_yueqifeng@ieggtb.com>
2021-12-22 10:06:57 +0800

    动态注册

--- app/src/main/java/com/example/aes/MainActivity.java
+++ app/src/main/java/com/example/aes/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aes;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn).setOnClickListener(view -> {
             String s = testJNI.encrypt("qf");
             Log.e("yue_", "encrypt: " + s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
         });
     }
 }
